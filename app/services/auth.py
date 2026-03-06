fa6bf1d1654d94836924dbdaa1b37a4ffaaa4c09
nvhao100604 <164126733+nvhao100604@users.noreply.github.com>
2026-03-06 12:25:07 +0700

    add domain

--- app/services/auth.py
+++ app/services/auth.py
@@ -132,7 +132,7 @@ def set_refresh_token_cookie(response: Response, refresh_token: str):
         samesite=samesite_val,
         max_age=settings.REFRESH_TOKEN_EXPIRE_DAYS * 24 * 3600,
         path="/",
-        domain=None
+        domain=cookie_domain
     )
 
 # CREATE NEW USER
